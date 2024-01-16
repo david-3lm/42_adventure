6f101864e34592f316ac0054be37119e75390900
David Lopez Lopez <dlopez-l@c2r12s3.42madrid.com>
2024-01-16 19:06:14 +0100

    pa casa

---
+++ libft/ft_strdup.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlopez-l <dlopez-l@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 18:18:55 by dlopez-l          #+#    #+#             */
+/*   Updated: 2024/01/16 18:23:40 by dlopez-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	char	*ptr;
+
+	len = ft_strlen(s1);
+	ptr = malloc(len);
+	return (ptr);
+}
