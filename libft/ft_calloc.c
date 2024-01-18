5a3b3095ad378509828a256768d9d54fbce28d9f
David Lopez Lopez <dlopez-l@c2r11s1.42madrid.com>
2024-01-18 16:10:19 +0100

    itoa

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -3,22 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+<<<<<<< HEAD
 /*   By: dlopez-l <dlopez-l@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 16:12:43 by dlopez-l          #+#    #+#             */
 /*   Updated: 2024/01/18 11:58:38 by dlopez-l         ###   ########.fr       */
+=======
+/*   By: dlopez-l <dlopez-l@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 16:12:43 by dlopez-l          #+#    #+#             */
+/*   Updated: 2024/01/16 16:25:31 by dlopez-l         ###   ########.fr       */
+>>>>>>> 5feaec38ab13f191be588b79c2176c7ec7201bca
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+<<<<<<< HEAD
+=======
+#include <stdlib.h>
+>>>>>>> 5feaec38ab13f191be588b79c2176c7ec7201bca
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 	char	*ptr_char;
+<<<<<<< HEAD
 
 	ptr = malloc(count * size);
 	ptr_char = ptr;
 	ft_bzero(ptr, count * size);
+=======
+	size_t	i;
+
+	i = 0;
+	ptr = malloc(count * size);
+	ptr_char = ptr;
+	while (i < count * size)
+	{
+		ptr_char[i] = 0;
+		i++;
+	}
+>>>>>>> 5feaec38ab13f191be588b79c2176c7ec7201bca
 	return (ptr);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,11 @@
 /*   By: dlopez-l <dlopez-l@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 16:46:39 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/01/18 12:31:20 by dlopez-l         ###   ########.fr       */
+<<<<<<< HEAD
+/*   Updated: 2024/01/18 15:24:31 by dlopez-l         ###   ########.fr       */
+=======
+/*   Updated: 2024/01/16 16:13:47 by dlopez-l         ###   ########.fr       */
+>>>>>>> 5feaec38ab13f191be588b79c2176c7ec7201bca
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +25,11 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
+<<<<<<< HEAD
 int		ft_tolower(int c);
+=======
+int		ft_toupper(int c);
+>>>>>>> 5feaec38ab13f191be588b79c2176c7ec7201bca
 int		ft_atoi(const char *nptr);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -35,10 +43,14 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+<<<<<<< HEAD
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char 	*ft_itoa(int n);
 char 	**ft_split(char const *s, char c);
+=======
+>>>>>>> 5feaec38ab13f191be588b79c2176c7ec7201bca
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *c);
