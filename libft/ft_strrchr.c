89f8ae902988694c5637a91e32f8e379a3859263
David Lopez Lopez <dlopez-l@c2r10s1.42madrid.com>
2024-01-25 13:06:14 +0100

    strrchr checks null

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: dlopez-l <dlopez-l@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 19:11:47 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/01/23 19:08:15 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/01/25 13:05:27 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strrchr(const char *s, int c)
 	int		i;
 	char	ch;
 
-	i = ft_strlen(s) - 1;
+	i = ft_strlen(s);
 	ch = (char)c;
 	while (i >= 0)
 	{
