bb4090db3933521e3bbe3d378d718981690d363c
David Lopez Lopez <dlopez-l@c2r10s1.42madrid.com>
2024-01-25 12:14:36 +0100

    strjoin fix

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dlopez-l <dlopez-l@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 15:56:25 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/01/18 12:29:36 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/01/25 12:13:03 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ptr = malloc(size_ptr);
 	if (!ptr)
 		return (0);
-	ft_strlcpy(ptr, s1, size_ptr);
-	ft_strlcat(ptr, s2, size_s1);
+	ft_strlcpy(ptr, s1, size_s1 + 1);
+	ft_strlcat(ptr, s2, size_ptr);
 	return (ptr);
 }
