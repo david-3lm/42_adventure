b3f92b0d008c8f8e5fd0a47c621b7cd2d4e07837
David Lopez Lopez <dlopez-l@c3r14s3.42madrid.com>
2024-01-26 17:28:42 +0100

    lstmap con segfault

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: dlopez-l <dlopez-l@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 23:57:23 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/01/26 13:13:53 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/01/26 17:04:58 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*aux;
-
-	aux = *lst;
-	while (aux)
-		aux = aux->next;
-	aux = new;
+	if (!*lst)
+		*lst = new;
+	else
+		ft_lstadd_back(&(*lst)->next, new);
 }
--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,50 +6,32 @@
 /*   By: dlopez-l <dlopez-l@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 12:02:16 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/01/26 12:51:18 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/01/26 17:27:41 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_list	*iter(t_list *l, int s, void *(*f)(void *), void (*del)(void *))
-{
-	int		i;
-	t_list	*start;
-	t_list	*cpy;
-	t_list	*link;
-
-	i = 1;
-	cpy = l;
-	start = ft_lstnew(cpy->content);
-	if (!start)
-		return (0);
-	link = start;
-	while (i < s)
-	{
-		cpy = cpy->next;
-		link->next = ft_lstnew(cpy->content);
-		if (!link->next)
-			ft_lstclear(&start, del);
-		link = link->next;
-		link->content = (*f)(link->content);
-		i++;
-	}
-	return (start);
-}
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	int		size;
-	int		i;
 	t_list	*start;
+	t_list	*aux;
 
-	size = ft_lstsize(lst);
-	i = 0;
-	if (!size || !lst)
-		return (0);
-	start = iter(lst, size, f, del);
+	start = ft_lstnew(f((lst)->content));
 	if (!start)
-		return (NULL);
+		return (0);
+	aux = start;
+	lst = lst->next;
+	while (lst)
+	{
+		aux->next = ft_lstnew(f((lst)->content));
+		if (!aux->next)
+		{
+			ft_lstclear(&aux, del);
+			return (0);
+		}
+		aux = aux->next;
+		lst = lst->next;
+	}
 	return (start);
 }
