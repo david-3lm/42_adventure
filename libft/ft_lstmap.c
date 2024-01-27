6ab8cdf64586fed50b6d741b92b22e59ce757e7c
david-3lm <d.3lopm@gmail.com>
2024-01-27 11:05:47 +0100

    lstmap fix

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -25,9 +25,9 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	while (lst)
 	{
 		aux->next = ft_lstnew(f((lst)->content));
-		if (!aux->next)
+		if (!(aux->next))
 		{
-			ft_lstclear(&aux, del);
+			ft_lstclear(&start, del);
 			return (0);
 		}
 		aux = aux->next;
