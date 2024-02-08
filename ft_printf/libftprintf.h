36defd0e6c80786158b31560563836c4e765757c
David Lopez Lopez <dlopez-l@c1r9s2.42madrid.com>
2024-02-08 20:04:55 +0100

    comment

--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: dlopez-l <dlopez-l@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 19:09:24 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/02/08 19:16:40 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/02/08 20:02:14 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 # define LIBFTPRINTF_H
 # include <stdio.h>
 # include <stdarg.h>
+# include <unistd.h>
 
 int	ft_printf(char const *format, ...);
-
+/*funciones para hacer:
+	-un write para cada tipo de dato (que entre void y se hará el cast, porque cuando no hay correlacion el resultado es indefinido)
+	-un detector de formato contador
+	-probablemente algo de conversion de bases
+*/
 #endif
