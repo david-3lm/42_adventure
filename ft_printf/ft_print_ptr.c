afd62e106026da65b8b71e4e1cbc8cdac6e7f3d1
David Lopez Lopez <dlopez-l@c1r15s4.42madrid.com>
2024-06-27 12:04:27 +0200

    end printf

--- ft_printf/ft_print_ptr.c
+++ ft_printf/ft_print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: dlopez-l <dlopez-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 15:52:33 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/06/20 14:58:45 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/06/27 12:03:46 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ int	ft_print_ptr(size_t ptr)
 		aux = write(1, "(nil)", 5);
 		if (aux == ERROR)
 			return (ERROR);
-		return (aux + 2);
+		return (aux);
 	}
-	aux = ft_print_hex(ptr, 0);
 	if (write(1, "0x", 2) == ERROR)
 		return (ERROR);
+	aux = ft_print_hex(ptr, 0);
 	if (aux == ERROR)
 		return (ERROR);
 	return (aux + 2);
---
+++ main_printf.c
@@ -0,0 +1,45 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	// Test case for character
+	ft_printf("Character: %c\n", 'A');
+	printf("Character: %c\n", 'A');
+
+	// Test case for string
+	ft_printf("String: %s\n", "Hello\nWorld\t!");
+	printf("String: %s\n", "Hello\nWorld\t!");
+
+	// Test case for decimal number
+	ft_printf("Decimal: %d\n", 12345);
+	printf("Decimal: %d\n", 12345);
+
+	// Test case for integer
+	ft_printf("Integer: %i\n", -12345);
+	printf("Integer: %i\n", -12345);
+
+
+	// Test case for unsigned integer
+	ft_printf("Unsigned: %u\n", 12345);
+	printf("Unsigned: %u\n", 12345);
+
+	// Test case for hexadecimal (lowercase)
+	ft_printf("Hexadecimal (lowercase): %x\n", 0);
+	printf("Hexadecimal (lowercase): %x\n", 0);
+
+	// Test case for hexadecimal (uppercase)
+	ft_printf("Hexadecimal (uppercase): %X\n", 255);
+	printf("Hexadecimal (uppercase): %X\n", 255);
+
+	// Test case for pointer
+	int num = 42;
+	ft_printf("Pointer: %p\n", NULL);
+	printf("Pointer: %p\n", NULL);
+
+	// Test case for percentage
+	ft_printf("Percentage: %%\n");
+	printf("Percentage: %%\n");
+
+	return 0;
+}
