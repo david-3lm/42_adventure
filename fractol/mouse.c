9c0427720d8bd6bdc1364fc1ee593a815a3669c3
David Lopez Lopez <dlopez-l@c3r11s1.42madrid.com>
2024-09-24 19:19:49 +0200

    Fractol mouse zoom

--- fractol/mouse.c
+++ fractol/mouse.c
@@ -6,7 +6,7 @@
 /*   By: dlopez-l <dlopez-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 19:11:05 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/09/23 11:10:59 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/09/24 14:43:11 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,13 @@ void	zoom(t_data *d, int x, int y, double zoom)
 
 	off_x = ((double)x / 800.0) - 0.5;
 	off_y = ((double)y / 800.0) - 0.5;
+	c.real = d->min_c.real - d->max_c.real;
+	c.imaginary = d->max_c.imaginary - d->min_c.imaginary;
 	if (zoom > 1)
 	{
 		off_x = -off_x;
 		off_y = -off_y;
 	}
-	ft_printf("%d, %d\n", x, y);
-	c.real = d->min_c.real - d->max_c.real;
-	c.imaginary = d->max_c.imaginary - d->min_c.imaginary;
 	d->max_c.real += ((c.real - zoom * c.real) / 2) + off_x;
 	d->min_c.real = d->max_c.real + zoom * c.real;
 	d->min_c.imaginary += ((c.imaginary - zoom * c.imaginary) / 2) - off_y;
@@ -36,18 +35,10 @@ void	zoom(t_data *d, int x, int y, double zoom)
 
 int	hook_mouse(int keycode, int x, int y, t_data *data)
 {
-	x = y;
-	y = x;
 	if (keycode == 4)
-	{
-		//ZOOM IN
 		zoom(data, x, y, 0.9);
-	}
 	else if (keycode == 5)
-	{
-		//ZOOM OUT
 		zoom(data, x, y, 1.1);
-	}
 	mlx_clear_window(data->mlx, data->win);
 	print_fractal(data);
 	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
