149cb7c06bc0aae936b35f327d71e58556ba8796
David Lopez Lopez <dlopez-l@c2r14s1.42madrid.com>
2024-09-27 16:50:16 +0200

    last norminette rule

--- fractol/fractal_utils.c
+++ fractol/fractal_utils.c
@@ -6,7 +6,7 @@
 /*   By: dlopez-l <dlopez-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 18:15:28 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/09/27 14:46:16 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/09/27 16:47:55 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,25 +34,16 @@ double	ft_atod(const char *nptr)
 	decimal_part = 0;
 	neg = 1;
 	divider = 1;
-	if (*nptr == '-')
-	{
+	if (*(nptr) == '-')
 		neg = -1;
-		nptr++;
-	}
-	integer_part = ft_atoi(nptr);
-	while (ft_isdigit(*nptr))
-	{
+	while (ft_isdigit(*++nptr))
 		integer_part = integer_part * 10 + (*nptr - '0');
-		nptr++;
-	}
 	if (*nptr == '.')
 	{
-		nptr++;
-		while (ft_isdigit(*nptr))
+		while (ft_isdigit(*(++nptr)))
 		{
 			decimal_part = decimal_part * 10 + (*nptr - '0');
 			divider *= 10;
-			nptr++;
 		}
 	}
 	return (neg * (integer_part + (decimal_part / divider)));
