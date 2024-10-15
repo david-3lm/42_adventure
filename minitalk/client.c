aead8ccbe9b8584f260c2bbe7d871af58106d25b
David Lopez Lopez <dlopez-l@c1r12s3.42madrid.com>
2024-10-15 19:59:28 +0200

    minitalk

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,8 +6,44 @@
 /*   By: dlopez-l <dlopez-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 16:18:00 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/10/15 16:20:29 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/10/15 19:56:51 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "ft_printf/ft_printf.h"
+
+void	action(int a)
+{
+	static int	received;
+
+	received = 0;
+	if (a == SIGUSR1)
+	{
+		printf("RECIBIDO SIGUSR1");
+	}
+	else
+	{
+		printf("%d\n", received);
+	}
+}
+
+void	ft_kill(int pid, char *str)
+{
+	(void)str;
+	kill(pid, SIGUSR1);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc != 3)
+		return (0);
+	signal(SIGUSR1, action);
+	ft_kill(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		pause();
+	return (0);
+}
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,24 +6,35 @@
 /*   By: dlopez-l <dlopez-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 16:18:08 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/10/15 18:07:16 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/10/15 19:57:24 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <unistd.h>
+#include <stdio.h>
+#include "ft_printf/ft_printf.h"
+
+void	action(int signal, siginfo_t *info, void *context)
+{
+	(void)context;
+	(void) info;
+	if (signal == SIGUSR1)
+	{
+		ft_printf("RECIBIDO SIGUSR1\n");
+	}
+}
 
 int	main(void)
 {
-	
+	struct sigaction	s_sigaction;
+
+	ft_printf("Server PID: %d\n", getpid());
+	s_sigaction.sa_sigaction = action;
+	s_sigaction.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
+	while (1)
+		pause();
+	return (0);
 }
-◦ signal
-◦ sigemptyset
-◦ sigaddset
-◦ sigaction
-◦ kill
-◦ getpid
-◦ pause
-◦ sleep
-◦ usleep
-◦ exit
