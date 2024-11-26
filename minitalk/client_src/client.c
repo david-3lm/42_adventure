40ed2b13507ddbc0b9229801761c865b07446503
David Lopez Lopez <d.3lopm@gmail.com>
2024-11-26 20:51:46 +0100

    changes??

--- minitalk/client_src/client.c
+++ minitalk/client_src/client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlopez-l <dlopez-l@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: dlopez-l <dlopez-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 16:18:00 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/11/13 00:09:50 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/11/26 20:10:49 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,7 @@ void	send_char(int pid, int c)
 		else
 			err = kill(pid, BIT0);
 		while (!g_client.ready_to_continue)
-			usleep(100);
+			usleep(1);
 		g_client.ready_to_continue = 0;
 		i--;
 		if (err)
