37a6ba995d171bd9e714b41d08d85342ee6f4abb
David <david@DESKTOP-E4LO8JI.localdomain>
2025-09-21 11:52:37 +0200

    cpp08

---
+++ cpps/cpp08/ex02/include/MutantStack.hpp
@@ -0,0 +1,48 @@
+
+#include <stack>
+
+#ifndef MUTANT_STACK_HPP
+#define MUTANT_STACK_HPP
+
+template <class T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack();
+        MutantStack(MutantStack const& other);
+        ~MutantStack();
+        MutantStack<T> &operator=(MutantStack const& other);
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin();
+        iterator end();
+};
+
+template<class T>
+MutantStack<T>::MutantStack() {}
+
+template<class T>
+MutantStack<T>::MutantStack(MutantStack const &other) : std::stack<T>(other) {}
+
+template<class T>
+MutantStack<T>::~MutantStack() {}
+
+template<class T>
+MutantStack<T> & MutantStack<T>::operator=(MutantStack const &other)
+{
+    return *this;
+}
+
+template<class T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+    return std::stack<T>::c.begin();
+}
+
+template<class T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+    return std::stack<T>::c.end();
+}
+
+#endif
